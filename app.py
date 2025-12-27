61c6aa85b44a4287fe9415d48325507ded672228
teslasf <bayrakta.h@gmail.com>
2025-12-27 22:12:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,14 @@ import os
 import atexit
 from file_operations import FileClass
 
-def create_app(config_path: str) -> Flask:
-    app = Flask(__name__)
-    app.config.from_object(config_path)
-    Routes(app)
-    Authentication(app)
-    return app
 
-def run_local_development():
-    """Run the app in local development mode"""
-    file_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'test')
-    app = create_app('config.Config')
-    FileClass(app, file_path)
-    atexit.register(cleanup)
-    app.run(debug=True)
+import os
+from flask import Flask # or from fastapi import FastAPI
 
-def run_production():
-    """Run the app in production mode"""
-    app = create_app("config.Config")
-    bucket_path = os.getenv("BUCKET_PATH", "path for the bucket")
-    FileClass(app, bucket_path)
-    app.run()
+app = Flask(__name__)
 
 if __name__ == "__main__":
-    if os.getenv('FLASK_ENV') == 'development':
-        run_local_development()
-    else:
-        run_production()
+    # Heroku provides the port via the PORT environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
